d2ba6c25825aa9e3ac81918651a7ce2f1c322b85
navkrishna <navkrishna@intelligrape.com>
2015-09-26 02:27:01 +0530

    Google+ integration done

--- app/src/main/java/in/buzzzz/activity/LoginActivity.java
+++ app/src/main/java/in/buzzzz/activity/LoginActivity.java
@@ -1,7 +1,9 @@
 package in.buzzzz.activity;
 
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.view.View;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -12,6 +14,13 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,11 +37,69 @@ import in.buzzzz.utility.Utility;
  */
 public class LoginActivity extends BaseActivity {
 
+    private static final int RC_SIGN_IN = 1001;
     private LoginButton mFacebookLoginButton;
+    private SignInButton mSignInButton;
     private CallbackManager mFacebookCallbackManager;
     private boolean isFacebookSdkInitialized = false;
     private static List<String> READ_PERMISSIONS = Arrays.asList("public_profile", "email");
     private static final String TAG = "LoginActivity";
+
+    private GoogleApiClient mGoogleApiClient;
+    /* Is there a ConnectionResult resolution in progress? */
+    private boolean mIsResolving = false;
+
+    /* Should we automatically resolve ConnectionResults when possible? */
+    private boolean mShouldResolve = false;
+
+    GoogleApiClient.ConnectionCallbacks mConnectionCallbacks = new GoogleApiClient.ConnectionCallbacks() {
+        @Override
+        public void onConnected(Bundle bundle) {
+            Logger.i(TAG, "onConnected:" + bundle);
+            mShouldResolve = false;
+
+            getGPlusProfileInfo();
+        }
+
+        @Override
+        public void onConnectionSuspended(int cause) {
+
+        }
+    };
+    GoogleApiClient.OnConnectionFailedListener mOnConnectionFailedListener = new GoogleApiClient.OnConnectionFailedListener() {
+        @Override
+        public void onConnectionFailed(ConnectionResult connectionResult) {
+            if (!mIsResolving && mShouldResolve) {
+                if (connectionResult.hasResolution()) {
+                    try {
+                        connectionResult.startResolutionForResult(mActivity, RC_SIGN_IN);
+                        mIsResolving = true;
+                    } catch (IntentSender.SendIntentException e) {
+                        Logger.e(TAG, "Could not resolve ConnectionResult.", e);
+                        mIsResolving = false;
+                        mGoogleApiClient.connect();
+                    }
+                } else {
+                    // Could not resolve the connection result, show the user an
+                    // error dialog.
+//                    showErrorDialog(connectionResult);
+                }
+            } else {
+                // Show the signed-out UI
+//                showSignedOutUI();
+            }
+        }
+    };
+    View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.sign_in_button_google_plus:
+                    onSignInClicked();
+                    break;
+            }
+        }
+    };
 
     private FacebookCallback<LoginResult> mFacebookCallback = new FacebookCallback<LoginResult>() {
         @Override
@@ -72,12 +139,12 @@ public class LoginActivity extends BaseActivity {
 
         @Override
         public void onCancel() {
-            // App code
+            Utility.showToastMessage(mActivity, "Login cancelled");
         }
 
         @Override
         public void onError(FacebookException exception) {
-            // App code
+            Utility.showToastMessage(mActivity, exception.getMessage());
         }
     };
 
@@ -86,11 +153,18 @@ public class LoginActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         initFacebook();
         setContentView(R.layout.activity_login);
-        mFacebookLoginButton = (LoginButton) findViewById(R.id.login_button_facebook);
-        mFacebookLoginButton.setReadPermissions(READ_PERMISSIONS);
+        initGoogleClient();
+        linkView();
+    }
 
-        // Callback registration
+    private void linkView() {
+        mFacebookLoginButton = (LoginButton) findViewById(R.id.login_button_facebook);
+        mSignInButton = (SignInButton) findViewById(R.id.sign_in_button_google_plus);
+
+        mFacebookLoginButton.setReadPermissions(READ_PERMISSIONS);
         mFacebookLoginButton.registerCallback(mFacebookCallbackManager, mFacebookCallback);
+
+        mSignInButton.setOnClickListener(mOnClickListener);
     }
 
     private void initFacebook() {
@@ -103,9 +177,69 @@ public class LoginActivity extends BaseActivity {
         mFacebookCallbackManager = CallbackManager.Factory.create();
     }
 
+    private void initGoogleClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(mConnectionCallbacks)
+                .addOnConnectionFailedListener(mOnConnectionFailedListener)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .build();
+    }
+
+    private void getGPlusProfileInfo() {
+        try {
+            if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+                Person currentPerson = Plus.PeopleApi
+                        .getCurrentPerson(mGoogleApiClient);
+                String googleDisplayName = currentPerson.getDisplayName();
+                String googlePersonEmail = Plus.AccountApi.getAccountName(mGoogleApiClient);
+                Logger.i(TAG, "Gender: " + currentPerson.getGender());
+                Logger.i(TAG, "Name: " + googleDisplayName + ", plusProfile: "
+                        + ", email: " + googlePersonEmail);
+            } else {
+                Utility.showToastMessage(mActivity, "Person information is null");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void onSignInClicked() {
+        // User clicked the sign-in button, so begin the sign-in process and automatically
+        // attempt to resolve any errors that occur.
+        mShouldResolve = true;
+        mGoogleApiClient.connect();
+
+        // Show a message to the user that we are signing in.
+//        mStatus.setText(R.string.signing_in);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
+        Logger.i(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // If the error resolution was not successful we should not resolve further.
+            if (resultCode != RESULT_OK) {
+                mShouldResolve = false;
+            }
+
+            mIsResolving = false;
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 }
--- app/src/main/java/in/buzzzz/utility/Logger.java
+++ app/src/main/java/in/buzzzz/utility/Logger.java
@@ -9,15 +9,21 @@ public final class Logger {
     private static final boolean DEBUG = true;
     private static final String TAG = "Buzzzz";
 
-    public static void i(String tag, String message) {
+    public static void i(String tag, Object message) {
         if (DEBUG) {
             Log.e(tag + "", "" + message);
         }
     }
 
-    public static void i(String message) {
+    public static void i(Object message) {
         if (DEBUG) {
             Log.e(TAG, "" + message);
+        }
+    }
+
+    public static void e(String tag, Object message, Throwable throwable) {
+        if (DEBUG) {
+            Log.e(tag, "" + message, throwable);
         }
     }
 }
