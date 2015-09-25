26b8670c4af82314055dcdfd25f9ec9add237460
navkrishna <navkrishna@intelligrape.com>
2015-09-25 21:53:41 +0530

    created callback for api response

---
+++ app/src/main/java/in/buzzzz/loader/APICaller.java
@@ -0,0 +1,10 @@
+package in.buzzzz.loader;
+
+import in.buzzzz.model.Model;
+
+/**
+ * Created by Navkrishna on September 25, 2015
+ */
+public interface APICaller {
+    void onComplete(Model model);
+}
