b79b3f6c39e7b5729cc477df3eba32f71a37d872
navkrishna <navkrishna@intelligrape.com>
2015-09-25 21:52:27 +0530

    created BaseActivity

---
+++ app/src/main/java/in/buzzzz/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package in.buzzzz.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Navkrishna on September 25, 2015
+ */
+public class BaseActivity extends AppCompatActivity {
+}
