c690ee0347946ceafeae05d6f68224d9e93836ec
navkrishna <navkrishna@intelligrape.com>
2015-09-27 02:07:31 +0530

    added autocomplete in create buzz

--- app/src/main/java/in/buzzzz/activity/CreateBuzzActivity.java
+++ app/src/main/java/in/buzzzz/activity/CreateBuzzActivity.java
@@ -3,17 +3,33 @@ package in.buzzzz.activity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import in.buzzzz.R;
+import in.buzzzz.loader.APICaller;
+import in.buzzzz.loader.LoaderCallback;
+import in.buzzzz.model.Interest;
+import in.buzzzz.model.InterestInfo;
+import in.buzzzz.model.Model;
+import in.buzzzz.model.Request;
+import in.buzzzz.parser.InterestParser;
+import in.buzzzz.utility.Api;
 import in.buzzzz.utility.ApiDetails;
+import in.buzzzz.utility.Utility;
 
 /**
  * Created by Navkrishna on September 26, 2015
@@ -24,9 +40,13 @@ public class CreateBuzzActivity extends BaseActivity {
     private EditText mEditTextBuzzTitle;
     private EditText mEditTextBuzzDesc;
     private TextView mTextViewBuzzVenue;
+    private TextView mTextViewSelectedInterests;
     private Button mButtonCreateBuzz;
     private CheckBox mCheckboxIsRsvp;
     private Spinner mSpinnerPeriod;
+    private List<Interest> mInterestList;
+    private AutoCompleteTextView mAutoCompleteTextViewInterest;
+    private JSONArray jsonArrayInterst = new JSONArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +56,7 @@ public class CreateBuzzActivity extends BaseActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.title_activity_create_buzz);
         linkViewsId();
+        requestInterest();
     }
 
     private void linkViewsId() {
@@ -46,8 +67,74 @@ public class CreateBuzzActivity extends BaseActivity {
         mButtonCreateBuzz = (Button) findViewById(R.id.button_create);
         mCheckboxIsRsvp = (CheckBox) findViewById(R.id.checkbox_is_rsvp);
         mSpinnerPeriod = (Spinner) findViewById(R.id.spinner_rsvp_options);
-
+        mAutoCompleteTextViewInterest = (AutoCompleteTextView) findViewById(R.id.autocompletetextview_interest);
+        mTextViewSelectedInterests = (TextView) findViewById(R.id.textview_selected_interests);
         mButtonCreateBuzz.setOnClickListener(mOnClickListener);
+    }
+
+    private void requestInterest() {
+        HashMap<String, String> params = new HashMap<>();
+        params.put(ApiDetails.REQUEST_KEY_NAME, ApiDetails.ACTION_NAME.INTEREST.name());
+        Request request = new Request(ApiDetails.ACTION_NAME.INTEREST);
+        request.setUrl(Api.BASE_URL_API + ApiDetails.ACTION_NAME.INTEREST.getActionName());
+        request.setShowDialog(true);
+        request.setDialogMessage(getString(R.string.progress_dialog_msg));
+        request.setParamMap(params);
+        request.setRequestType(Request.HttpRequestType.GET);
+        LoaderCallback loaderCallback = new LoaderCallback(mActivity, new InterestParser());
+        boolean hasNetwork = loaderCallback.requestToServer(request);
+        loaderCallback.setServerResponse(new APICaller() {
+
+            @Override
+            public void onComplete(Model model) {
+
+                if (model.getStatus() == 1) {
+                    if (model instanceof InterestInfo) {
+                        InterestInfo interestInfo = (InterestInfo) model;
+                        mInterestList = interestInfo.getInterestList();
+                        setDataAutoListAdapter();
+                    }
+                }
+            }
+        });
+        if (!hasNetwork) {
+            Utility.showToastMessage(mActivity, getString(R.string.no_network));
+        }
+    }
+
+    private static final String KEY_INTEREST_ID = "key_interest_id";
+    private static final String KEY_INTEREST_NAME = "key_interest_name";
+
+    private void setDataAutoListAdapter() {
+        List<HashMap<String, String>> countryList = new ArrayList<HashMap<String, String>>();
+        for (int i = 0; i < mInterestList.size(); i++) {
+            HashMap<String, String> interestMap = new HashMap<String, String>();
+            Interest interest = mInterestList.get(i);
+            interestMap.put(KEY_INTEREST_ID, interest.getId());
+            interestMap.put(KEY_INTEREST_NAME, interest.getName());
+            countryList.add(interestMap);
+        }
+        String[] from = {KEY_INTEREST_NAME};
+        int[] to = {R.id.textview_custom_interest};
+
+        SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), countryList, R.layout.layout_custom_autocomplete, from, to);
+        mAutoCompleteTextViewInterest.setAdapter(adapter);
+        mAutoCompleteTextViewInterest.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                HashMap<String, String> interestMap = (HashMap<String, String>) parent.getAdapter().getItem(position);
+                for (Interest interest : mInterestList) {
+                    if (interest.getName().equals(interestMap.get(KEY_INTEREST_NAME))) {
+                        jsonArrayInterst.put(interest.getName());
+                        if (jsonArrayInterst.length() > 1)
+                            mTextViewSelectedInterests.append(", ");
+                        mTextViewSelectedInterests.append(interest.getName());
+                        mAutoCompleteTextViewInterest.setText("");
+                        break;
+                    }
+                }
+            }
+        });
     }
 
     private void requestCreatBuzz() {
@@ -62,15 +149,18 @@ public class CreateBuzzActivity extends BaseActivity {
         params.put(ApiDetails.REQUEST_KEY_END_TIME, "");
         params.put(ApiDetails.REQUEST_KEY_PERIOD, ApiDetails.PERIOD.ONCE.name());
         params.put(ApiDetails.REQUEST_KEY_TAGS, ""); // json array
-        params.put(ApiDetails.REQUEST_KEY_INTERESTS, ""); // json array
+        params.put(ApiDetails.REQUEST_KEY_INTERESTS, jsonArrayInterst.toString()); // json array
     }
 
     private View.OnClickListener mOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             String buzzTitle = mEditTextBuzzTitle.getText().toString().trim();
-            String buzzDesc = mEditTextBuzzDesc.getText().toString().trim();
-            if (!buzzTitle.isEmpty() && !buzzDesc.isEmpty()) {
+            if (buzzTitle.isEmpty()) {
+                mEditTextBuzzTitle.setError("Enter title");
+            } else if (jsonArrayInterst.length() == 0) {
+                mAutoCompleteTextViewInterest.setError("Select at least one interest");
+            } else {
                 requestCreatBuzz();
             }
         }
